fd05016cc0734eaae7a92bdfc4223dd255f1fd6b
Irina <70594426+irinazheltisheva@users.noreply.github.com>
2021-06-01 17:59:57 +0300

    Initial commit

---
+++ src/ru/geekbrains/lesson1/Main.java
@@ -0,0 +1,27 @@
+package ru.geekbrains.lesson1;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b,
+        // и инициализируйте их любыми значениями, которыми захотите.
+        // Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”,
+        // в противном случае “a < b”;
+
+        compareNumbers();
+    }
+
+    public static void compareNumbers() {
+        int a = 0;
+        int b = 0;
+        if (a >= b) {
+            ;
+            System.out.println("a >= b");
+
+        } else {
+
+            System.out.println("a < b");
+        }
+
+    }
+}
